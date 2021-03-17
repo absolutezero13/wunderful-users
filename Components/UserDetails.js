2f9dc0f91260ba028634be719e41dc8117fad803
erol-sarica <ilker897@gmail.com>
2021-03-17 19:35:00 +0300

    usefocus fail

--- Components/UserDetails.js
+++ Components/UserDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import Icon from "react-native-vector-icons/FontAwesome";
 const UserDetails = ({ navigation }) => {
@@ -7,31 +7,24 @@ const UserDetails = ({ navigation }) => {
   const firstName = navigation.getParam("name").first;
   const lastName = navigation.getParam("name").last;
   const picture = navigation.getParam("picture").large;
+  const gender = navigation.getParam("gender");
   const lat = navigation.getParam("location").coordinates.latitude;
   const long = navigation.getParam("location").coordinates.longitude;
-  const gender = navigation.getParam("gender");
+  const country = navigation.getParam("location").country;
+  const state = navigation.getParam("location").state;
 
   return (
-    <View style={{}}>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          padding: 30,
-          paddingVertical: 30,
-        }}
-      >
+    <View style={{ backgroundColor: "white" }}>
+      <View style={styles.user}>
         <Image
           source={{
             uri: picture,
           }}
-          style={{ width: 100, height: 100, borderRadius: 50 }}
+          style={styles.userImage}
         />
         <View style={{ marginLeft: 25 }}>
-          <Text style={{ color: "#b1b8c5", fontSize: 16 }}>Name</Text>
-          <Text style={{ color: "#6374f3", fontSize: 20, fontWeight: "bold" }}>
-            {firstName + " " + lastName}
-          </Text>
+          <Text style={styles.userInfoText}>Name</Text>
+          <Text style={styles.nameText}>{firstName + " " + lastName}</Text>
         </View>
       </View>
       <MapView
@@ -41,45 +34,84 @@ const UserDetails = ({ navigation }) => {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-        style={{
-          width: "100%",
-          height: 300,
-        }}
+        style={styles.map}
       >
-        <Marker coordinate={{ latitude: +lat, longitude: +long }} />
+        <Marker
+          coordinate={{ latitude: +lat, longitude: +long }}
+          title={state}
+          description={country}
+        />
       </MapView>
-      <Text
-        style={{
-          position: "absolute",
-          top: 170,
-          left: 10,
-          color: "#b1b8c5",
-          fontWeight: "bold",
-          fontSize: 16,
-        }}
-      >
-        Location
-      </Text>
-      <View style={{ flexDirection: "row" }}>
-        <View style={{ height: 220, width: "40%", backgroundColor: "red" }}>
-          <Text style={{ color: "#b1b8c5", fontWeight: "bold", fontSize: 16 }}>
-            Gender
-          </Text>
-
+      <Text style={styles.locationText}>Location</Text>
+      <View style={{ flexDirection: "row", height: "30%" }}>
+        <View style={styles.genderContainer}>
+          <Text style={styles.userInfoText}>Gender</Text>
           {gender === "female" ? (
-            <Icon name="venus" size={30} color="#b1b8c5" />
+            <Icon style={styles.genderIcon} name="venus" size={50} />
           ) : (
-            <Icon name="mars" size={30} color="#b1b8c5" />
+            <Icon style={styles.genderIcon} name="mars" size={50} />
           )}
         </View>
-
-        <View style={{ width: "60%", backgroundColor: "yellow" }}>
-          <Text style={{ color: "#b1b8c5" }}>Age</Text>
+        <View style={styles.ageContainer}>
+          <Text style={styles.userInfoText}>Age</Text>
           <Text> {age} </Text>
         </View>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  user: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 30,
+    paddingVertical: 30,
+  },
+  userImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+
+  userInfoText: {
+    color: "#b1b8c5",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  nameText: {
+    color: "#6374f3",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  map: {
+    width: "100%",
+    height: 300,
+  },
+  locationText: {
+    position: "absolute",
+    top: 170,
+    left: 10,
+    color: "#b1b8c5",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  genderContainer: {
+    padding: 10,
+    width: "35%",
+    backgroundColor: "white",
+  },
+  ageContainer: {
+    padding: 10,
+    width: "65%",
+    backgroundColor: "yellow",
+  },
+  genderIcon: {
+    alignSelf: "center",
+    marginTop: 30,
+    color: "#6374f3",
+  },
+});
 
 export default UserDetails;
--- Components/UsersPage.js
+++ Components/UsersPage.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, Image } from "react-native";
+import React, { useEffect, useState, useCallback } from "react";
+import { StyleSheet, View, Text, Image, Button } from "react-native";
 import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import Icon from "react-native-vector-icons/FontAwesome";
-
+import { useFocusEffect } from "@react-navigation/native";
 const UsersPage = ({ navigation }) => {
   const [users, setUsers] = useState([]);
   const [visible, setVisible] = useState(true);
   useEffect(() => {
-    const getUser = () =>
+    const getUser = () => {
       fetch("https://randomuser.me/api")
         .then((res) => res.json())
         .then((data) =>
@@ -18,28 +18,40 @@ const UsersPage = ({ navigation }) => {
             ]);
           })
         );
+
+      console.log("cleanup?");
+    };
     getUser();
-    //setInterval(getUser, 10000);
-    return () => clearInterval(getUser);
+    //const userInterval = setInterval(getUser, 5000);
+    return () => clearInterval(userInterval);
   }, []);
+
+  // useFocusEffect(
+  //   useCallback(() => {
+  //     const getUser = () => {
+  //       fetch("https://randomuser.me/api")
+  //         .then((res) => res.json())
+  //         .then((data) =>
+  //           setUsers((prevUsers) => {
+  //             return setUsers([
+  //               ...prevUsers,
+  //               { key: data.results[0].login.uuid, value: data.results[0] },
+  //             ]);
+  //           })
+  //         );
+
+  //       console.log("cleanup?");
+  //     };
+  //     getUser();
+  //     const userInterval = setInterval(getUser, 5000);
+  //     return () => clearInterval(userInterval);
+  //   }, [])
+  // );
   return (
     <View>
-      <View
-        style={{
-          flexDirection: "row",
-          paddingHorizontal: 20,
-          paddingVertical: 15,
-        }}
-      >
-        <Text
-          style={{
-            color: "#b1b8c5",
-            fontSize: 25,
-          }}
-        >
-          PROFILES
-        </Text>
-
+      <View style={styles.header}>
+        <Text style={styles.headerText}>PROFILES</Text>
+        <Button title="delete" onPress={() => setUsers([])} />
         <Icon
           onPress={() => setVisible(!visible)}
           name="bars"
@@ -56,32 +68,14 @@ const UsersPage = ({ navigation }) => {
             <TouchableOpacity
               onPress={() => navigation.navigate("userDetails", item.value)}
             >
-              <View
-                style={{
-                  flex: 1,
-                  paddingHorizontal: 20,
-                  paddingVertical: 15,
-                  marginBottom: 5,
-                  flexDirection: "row",
-                  alignItems: "center",
-                  borderBottomWidth: 1,
-                  borderBottomColor: "#dbe0e5",
-                }}
-              >
+              <View style={styles.user}>
                 <Image
                   source={{
-                    uri: item.value.picture.thumbnail,
+                    uri: item.value.picture.medium,
                   }}
-                  style={{ width: 52, height: 52, borderRadius: 50 }}
+                  style={styles.userImage}
                 />
-                <Text
-                  style={{
-                    color: "#6374f3",
-                    fontWeight: "bold",
-                    marginLeft: 10,
-                    fontSize: 20,
-                  }}
-                >
+                <Text style={styles.username}>
                   {item.value.name.first + ", " + item.value.dob.age}
                 </Text>
                 <Icon
@@ -106,5 +100,38 @@ const UsersPage = ({ navigation }) => {
     </View>
   );
 };
-
+//// STYLES ////
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  headerText: {
+    color: "#b1b8c5",
+    fontSize: 25,
+  },
+  user: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    paddingBottom: 20,
+    marginBottom: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#dbe0e5",
+  },
+  userImage: {
+    width: 52,
+    height: 52,
+    borderRadius: 50,
+  },
+  username: {
+    color: "#6374f3",
+    fontWeight: "bold",
+    marginLeft: 10,
+    fontSize: 20,
+  },
+});
 export default UsersPage;
